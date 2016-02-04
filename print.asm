69a426968ebf805b0db15a416836418b9e5def59
meutraa <paul@lost.host>
2016-02-04 08:46:15 +0200

    Print routine now preserves all registers and has a macro

--- print.asm
+++ print.asm
@@ -1,53 +1,57 @@
 %define SYS_WRITE 1
 %define stdout 1
 
-_print:
-    push rbp
-    push rax
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
+%macro print 1
+    mov qword [reg + 0], %1
+    call _print
+%endmacro
 
-    mov rax, rbx
-    mov r9, 10
-    mov r12, 1
-    push 0Ah
+section .bss
+    save resq 8
+    reg resq 1
+
+section .text
+
+_print:
+    ; save registers
+    mov qword [save + 0], rax
+    mov qword [save + 8], rcx
+    mov qword [save + 16], rdx
+    mov qword [save + 24], rdi
+    mov qword [save + 32], rsi
+    mov qword [save + 40], r10
+    mov qword [save + 48], r11
+    mov qword [save + 56], r12
+
+    ; begin print
+    mov rax, qword [reg + 0]
+    mov rcx, 10
+    mov r12, 2
+    push word 0Ah
 
 .div:
     xor rdx, rdx
-    div r9
+    div rcx
     add rdx, '0'
-    inc r12
-    push rdx 
+    push word dx
+    add r12, 2
     cmp rax, 0
     jne .div
     
+    mov rdx, r12 
     mov rax, SYS_WRITE
     mov rdi, stdout
-    mov rdx, 1 
-
-.print: 
     mov rsi, rsp
     syscall
-    add rsp, 8
-    dec r12
-    jnz .print
+    add rsp, r12
 
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rax
-    pop rbp
+    ; load registers
+    mov rax, qword [save + 0]
+    mov rcx, qword [save + 8]
+    mov rdx, qword [save + 16]
+    mov rdi, qword [save + 24]
+    mov rsi, qword [save + 32]
+    mov r10, qword [save + 40]
+    mov r11, qword [save + 48]
+    mov r12, qword [save + 56]
 ret
